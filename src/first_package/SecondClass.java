5f6615584d6e29be7f2bb8ecc942550d871bc1a0
nmuth23 <114309863+nmuth23@users.noreply.github.com>
2023-01-07 14:31:50 -0500

    Update SecondClass.java

--- src/first_package/SecondClass.java
+++ src/first_package/SecondClass.java
@@ -3,5 +3,8 @@ package first_package;
 public class SecondClass {
     public static void main(String[] args) {
         System.out.println("first");
+        
+        //add some changes
+         System.out.println("first 1");
     }
 }
